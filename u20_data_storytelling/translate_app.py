fe6bbfd19efb42f084c3c7ae0a34fed37d6a653a
jonnross88 <68479485+jonnross88@users.noreply.github.com>
2023-10-26 18:49:10 -0500

    changes translate_app method

--- u20_data_storytelling/translate_app.py
+++ u20_data_storytelling/translate_app.py
@@ -1,5 +1,5 @@
-import asyncio
-from collections.abc import MutableSequence, Iterable
+from collections.abc import Iterable
+# import asyncio
 
 # Imports the Google Cloud Translation library
 from google.cloud import translate
@@ -10,8 +10,7 @@ def translate_list(
     project_id: str = "mrprimetranslator",
     source_lang: str = "de",
     target_lang: str = "en-US",
-):
-    trans_dict = {}
+) -> translate.TranslateTextResponse:
     client = translate.TranslationServiceClient()
     location = "global"
     parent = f"projects/{project_id}/locations/{location}"
@@ -24,6 +23,15 @@ def translate_list(
             "target_language_code": target_lang,
         }
     )
+    return response
+
+
+def translation_response_to_dict(
+    list_of_strings: Iterable[str], response: translate.TranslateTextResponse
+) -> dict:
+    """Converts a TranslateTextResponse object to a dictionary 
+    with the original text as key and the translated text as value."""
+    trans_dict = {}
     trans_dict = {
         text: translation.translated_text
         for (text, translation) in zip(list_of_strings, response.translations)
@@ -32,49 +40,60 @@ def translate_list(
     return trans_dict
 
 
+def translate_list_to_dict(
+    list_of_strings: Iterable[str],
+    project_id: str = "mrprimetranslator",
+    source_lang: str = "de",
+    target_lang: str = "en-US",
+) -> dict:
+    """Translates a list of strings to a dictionary with the original text as key and the translated text as value."""
+    response = translate_list(list_of_strings, project_id, source_lang, target_lang)
+    return translation_response_to_dict(list_of_strings, response)
+
+
 # Initialize Translation client
-async def translate_text_async(
-    text: str = "YOUR_TEXT_TO_TRANSLATE", project_id: str = "YOUR_PROJECT_ID"
-) -> translate.TranslationServiceClient:
-    """Translating Text."""
+# async def translate_text_async(
+#     text: str = "YOUR_TEXT_TO_TRANSLATE", project_id: str = "YOUR_PROJECT_ID"
+# ) -> translate.TranslationServiceClient:
+#     """Translating Text."""
 
-    client = translate.TranslationServiceClient()
+#     client = translate.TranslationServiceClient()
 
-    location = "global"
+#     location = "global"
 
-    parent = f"projects/{project_id}/locations/{location}"
+#     parent = f"projects/{project_id}/locations/{location}"
 
-    # Translate text from German to English
-    # Detail on supported types can be found here:
-    # https://cloud.google.com/translate/docs/supported-formats
-    response = client.translate_text(
-        request={
-            "parent": parent,
-            "contents": [text],
-            "mime_type": "text/plain",  # mime types: text/plain, text/html
-            "source_language_code": "de",
-            "target_language_code": "en-US",
-        }
-    )
+#     # Translate text from German to English
+#     # Detail on supported types can be found here:
+#     # https://cloud.google.com/translate/docs/supported-formats
+#     response = client.translate_text(
+#         request={
+#             "parent": parent,
+#             "contents": [text],
+#             "mime_type": "text/plain",  # mime types: text/plain, text/html
+#             "source_language_code": "de",
+#             "target_language_code": "en-US",
+#         }
+#     )
 
-    # Display the translation for each input text provided
-    for translation in response.translations:
-        # print(f"Translated text: {translation.translated_text}")
-        return translation.translated_text
+#     # Display the translation for each input text provided
+#     for translation in response.translations:
+#         # print(f"Translated text: {translation.translated_text}")
+#         return translation.translated_text
 
 
-async def translate_text_parallel(
-    text_list, project_id: str = "YOUR_PROJECT_ID"
-) -> translate.TranslationServiceClient:
-    """Translates a list of texts in parallel using the Google Cloud Translation API asynchronously."""
-    # Create a list of tasks
-    tasks = [translate_text_async(text, project_id) for text in text_list]
+# async def translate_text_parallel(
+#     text_list, project_id: str = "YOUR_PROJECT_ID"
+# ) -> translate.TranslationServiceClient:
+#     """Translates a list of texts in parallel using the Google Cloud Translation API asynchronously."""
+#     # Create a list of tasks
+#     tasks = [translate_text_async(text, project_id) for text in text_list]
 
-    # Execute the tasks concurrently
-    results = await asyncio.gather(*tasks)
+#     # Execute the tasks concurrently
+#     results = await asyncio.gather(*tasks)
 
-    # Return the translated texts
-    return results
+#     # Return the translated texts
+#     return results
 
 
 # translate_text('Afghanischer Windhund', 'mrprimetranslator')

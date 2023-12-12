9955f4b4a3d53743c21c6bbbe7b7d2872ad7ff0f
jonnross88 <68479485+jonnross88@users.noreply.github.com>
2023-12-12 11:34:54 -0600

    merge before rerunning stats.

--- u20_data_storytelling/translate_app.py
+++ u20_data_storytelling/translate_app.py
@@ -55,50 +55,5 @@ def translate_list_to_dict(
     return translation_response_to_dict(list_of_strings, response)
 
 
-# Initialize Translation client
-# async def translate_text_async(
-#     text: str = "YOUR_TEXT_TO_TRANSLATE", project_id: str = "YOUR_PROJECT_ID"
-# ) -> translate.TranslationServiceClient:
-#     """Translating Text."""
-
-#     client = translate.TranslationServiceClient()
-
-#     location = "global"
-
-#     parent = f"projects/{project_id}/locations/{location}"
-
-#     # Translate text from German to English
-#     # Detail on supported types can be found here:
-#     # https://cloud.google.com/translate/docs/supported-formats
-#     response = client.translate_text(
-#         request={
-#             "parent": parent,
-#             "contents": [text],
-#             "mime_type": "text/plain",  # mime types: text/plain, text/html
-#             "source_language_code": "de",
-#             "target_language_code": "en-US",
-#         }
-#     )
-
-#     # Display the translation for each input text provided
-#     for translation in response.translations:
-#         # print(f"Translated text: {translation.translated_text}")
-#         return translation.translated_text
-
-
-# async def translate_text_parallel(
-#     text_list, project_id: str = "YOUR_PROJECT_ID"
-# ) -> translate.TranslationServiceClient:
-#     """Translates a list of texts in parallel using the Google Cloud Translation API asynchronously."""
-#     # Create a list of tasks
-#     tasks = [translate_text_async(text, project_id) for text in text_list]
-
-#     # Execute the tasks concurrently
-#     results = await asyncio.gather(*tasks)
-
-#     # Return the translated texts
-#     return results
-
-
 # translate_text('Afghanischer Windhund', 'mrprimetranslator')
 translate_list_to_dict(["Schäferhund", "Dackel", "Rottweiler", "Afghanischer Windhund"])
